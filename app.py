297beba2719644056ee85a9e53f569f9d9a1d060
Ritik Barnwal <theritikbarnwal@gmail.com>
2025-09-02 21:45:30 +0530

    update app.py

--- app.py
+++ app.py
@@ -220,4 +220,4 @@ def unauthorized(error):
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000,debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True) 
